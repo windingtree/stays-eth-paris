5bb5e2a491b3136abebfb88209ab22cdca903674
bobetbat <b.bobetayev.97@gmail.com>
2022-06-22 19:18:56 +0200

    fix(dapp): validate mobile browser

--- packages/dapp/src/pages/PageWrapper.tsx
+++ packages/dapp/src/pages/PageWrapper.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import { useEffect, useState } from 'react';
 import type { Breadcrumb } from '../components/Breadcrumbs';
 import { useContext } from 'react';
 import { Anchor, Box, ResponsiveContext } from 'grommet';
@@ -6,7 +7,7 @@ import { useAppState } from '../store';
 import { Breadcrumbs } from '../components/Breadcrumbs';
 import { MessageBox } from '../components/MessageBox';
 import { getNetwork } from '../config';
-import { isMobile, isSafari } from 'react-device-detect';
+import { isMobile, browserName } from 'react-device-detect';
 
 const { name: allowedNetworkName } = getNetwork();
 
@@ -18,6 +19,34 @@ export interface PageWrapperProps {
 export const PageWrapper = ({ children, breadcrumbs }: PageWrapperProps) => {
   const size = useContext(ResponsiveContext);
   const { isRightNetwork } = useAppState();
+  const [isSupported, setIsSupported] = useState(false)
+
+  useEffect(() => {
+    if (window.ethereum) {
+      handleEthereum();
+    } else {
+      window.addEventListener('ethereum#initialized', handleEthereum, {
+        once: true,
+      });
+
+      // If the event is not dispatched by the end of the timeout,
+      // the user probably doesn't have MetaMask installed.
+      setTimeout(handleEthereum, 3000); // 3 seconds
+    }
+
+    function handleEthereum() {
+      const { ethereum } = window;
+      if (ethereum && ethereum.isMetaMask) {
+        console.log('Ethereum successfully detected!');
+        // Access the decentralized web!
+        setIsSupported(true)
+      } else {
+        setIsSupported(false)
+        console.log('Please install MetaMask!');
+      }
+    }
+  }, [])
+
   return (
     <Box>
       <Box
@@ -32,11 +61,11 @@ export const PageWrapper = ({ children, breadcrumbs }: PageWrapperProps) => {
         <MessageBox type='warn' show={!isRightNetwork}>
           You are connected to a wrong network. Please switch to: {allowedNetworkName}
         </MessageBox>
-        <MessageBox type='warn' show={isMobile}>
-          Browser does not support dApp. Open dApp with <Anchor label='metamask' href={`https://metamask.app.link/dapp/${window.location.href}`} />
+        <MessageBox type='warn' show={!isSupported && isMobile}>
+          Mobile {browserName} does not support web3 apps. Open with <Anchor label='metamask' href={`https://metamask.app.link/dapp/${window.location.href}`} />
         </MessageBox>
-        <MessageBox type='warn' show={isSafari && !isMobile}>
-          Browser does not support dApp. <Anchor label='Check options' href='https://metamask.io' />
+        <MessageBox type='warn' show={!isSupported && !isMobile}>
+          {browserName} does not support web3 apps. <Anchor label='Check options' href='https://metamask.io' />
         </MessageBox>
         {children}
       </Box>
